068008671d298e2984da13a2a57e704866320e78
sarajit <bdsarajit499@gamil.com>
2024-09-19 23:27:36 +0600

    add new

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ import React from "react";
 function HomePage() {
   return (
     <div>
-      <h2 className="text-center text-red-500">home page design coming soon</h2>
+      <h2 className="text-center text-red-500">
+        home page design coming soon
+        add new line
+        </h2>
     </div>
   );
 }
