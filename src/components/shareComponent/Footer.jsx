904c7e3b5127fdea56b5ff579ee2b7637a557c89
mdmahadihasancse <mdmahadihasancse@gmail.com>
2024-09-19 14:22:16 +0600

    test

--- src/components/shareComponent/Footer.jsx
+++ src/components/shareComponent/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-         <h1 className='text-4xl'> this is ashraful islam tusar added</h1>
+         <h1 className='text-4xl'> this is ashraful islam tusar vai</h1>
         </div>
     );
 };
