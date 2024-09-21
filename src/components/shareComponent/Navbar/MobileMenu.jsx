aad361cb3d14466d8026b74b0fae2554b25bd5e0
Sarajit-mondal <bdsarajit499@gmail.com>
2024-09-21 20:58:19 +0600

    add profile with mobileMenu

--- src/components/shareComponent/Navbar/MobileMenu.jsx
+++ src/components/shareComponent/Navbar/MobileMenu.jsx
@@ -2,18 +2,29 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
 import { ImCross } from "react-icons/im";
 import { RiMenuUnfold2Line } from "react-icons/ri";
+import Profile from "./Profile";
 
 const MobileMenu = () => {
+  const [isDropDown, setIsDropDown] = useState();
+  // Function to handle the dropdown toggle
+  const toggleDropdown = () => {
+    setIsDropDown(!isDropDown);
+  };
   const [display, setDisplay] = useState(false);
+  const displayOnOff = () => {
+    setDisplay(!display);
+    setIsDropDown(false);
+  };
   return (
-    <menu className="md:hidden z-30">
+    <menu className="md:hidden z-30 h-full">
       {/* Add flexbox and justify-end to align the button on the right */}
-      
+
       <div className="flex justify-end">
         <button
-          onClick={() => setDisplay(!display)}
+          onClick={() => displayOnOff()}
           className="bg-blue-600 font-bold text-xl text-white px-2 py-1 rounded-md"
         >
           <RiMenuUnfold2Line />
@@ -23,7 +34,7 @@ const MobileMenu = () => {
       <ul
         className={`${
           display ? "block" : "hidden"
-        } absolute bg-blue-600 min-h-screen top-0 right-0 transition-all text-white`}
+        } absolute bg-blue-600 min-h-screen top-0 right-0 transition-all text-white pb-5`}
       >
         <button
           onClick={() => setDisplay(false)}
@@ -32,20 +43,39 @@ const MobileMenu = () => {
           <ImCross className="text-2xl" />
         </button>
         <hr className="text-xl font-bold h-2 mb-3" />
-        <li className="hover:text-green-500 px-6 transition-all">
+        <li
+          onClick={() => displayOnOff()}
+          className="md:hover:text-blue-300 px-6 transition-all"
+        >
           <Link href="/">Home</Link>
         </li>
-        <li className="hover:text-green-500 px-6 transition-all">
+        <li
+          onClick={() => displayOnOff()}
+          className="md:hover:text-blue-300 px-6 transition-all"
+        >
           <Link href="/about">About</Link>
         </li>
-        <li className="hover:text-green-500 px-6 transition-all">
+        <li
+          onClick={() => displayOnOff()}
+          className="md:hover:text-blue-300 px-6 transition-all"
+        >
           <Link href="/contact">Contacts</Link>
         </li>
-        <li className="hover:text-green-500 px-6 transition-all">
+        <li
+          onClick={() => displayOnOff()}
+          className="md:hover:text-blue-300 px-6 transition-all"
+        >
           <Link href="/pricing">Pricing</Link>
         </li>
+        {/* profile dropdown */}
+        <div onClick={toggleDropdown}>
+          <li className="md:hover:text-blue-300 px-6 transition-all mt-2 cursor-pointer ">
+            {!isDropDown && <CgProfile className="text-2xl mx-auto" />}
+          </li>
+          {isDropDown && <Profile />}
+        </div>
         <hr className="mt-5" />
-        <li className="py-2 mt-5 px-4 bg-green-700 border-0 mx-4 text-white rounded-md hover:text-white hover:scale-105 transition-all">
+        <li className="py-2 mt-5 px-4 bg-blue-700 border-0 mx-4 text-white rounded-md hover:text-white hover:scale-105 transition-all text-center">
           <button>Login</button>
         </li>
       </ul>
