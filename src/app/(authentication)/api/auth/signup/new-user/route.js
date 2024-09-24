fa1882459311623168aa6453a9412663a56eddce
Sarajit-mondal <bdsarajit499@gmail.com>
2024-09-24 20:11:56 +0600

    mongodb connect

--- src/app/(authentication)/api/auth/signup/new-user/route.js
+++ src/app/(authentication)/api/auth/signup/new-user/route.js
@@ -1,18 +1,18 @@
-// import connectDB from "@/lib/connectDB";
+import connectDB from "@/lib/connectDB";
 
-// export const POST = async (res) => {
-//   try {
-//     const db = await connectDB();
-//     //    connection name
-//     const userCollection = db.collection("users");
+export const POST = async (res) => {
+  try {
+    const db = await connectDB();
+    //    connection name
+    const userCollection = db.collection("users");
 
-//     const newUser = await res.json();
-//     const res = await userCollection.insertOne(newUser);
-//     return Response.json({ message: "new user Created" });
-//   } catch (error) {
-//     return Response.json({ message: "Something went wrong" });
-//   }
-// }
+    const newUser = await res.json();
+    const res = await userCollection.insertOne(newUser);
+    return Response.json({ message: "new user Created" });
+  } catch (error) {
+    return Response.json({ message: "Something went wrong" });
+  }
+};
 
 export const GET = () => {
   return "yess get data";
