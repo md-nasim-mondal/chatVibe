14c7056edd5487d2eb12ff88ec0e296b8f56374f
Md Atikuzzaman <akash203037@gmail.com>
2024-09-26 23:32:16 +0600

    some code on banner section modified

--- src/components/shareComponent/Navbar/MobileMenu.jsx
+++ src/components/shareComponent/Navbar/MobileMenu.jsx
@@ -18,8 +18,7 @@ const MobileMenu = () => {
       <div className="flex justify-end">
         <button
           onClick={() => displayOnOff()}
-          className="bg-blue-600 font-bold text-xl text-white px-2 py-1 rounded-md"
-        >
+          className="bg-blue-600 font-bold text-xl text-white px-2 py-1 rounded-md">
           <RiMenuUnfold2Line />
         </button>
       </div>
@@ -27,44 +26,38 @@ const MobileMenu = () => {
       <ul
         className={`${
           display ? "block" : "hidden"
-        } absolute bg-blue-600 min-h-screen top-0 right-0 transition-all text-white pb-5`}
-      >
+        } absolute bg-blue-600 min-h-screen top-0 right-0 transition-all text-white pb-5`}>
         <button
           onClick={() => setDisplay(false)}
-          className="hover:text-red-500 p-6 mb-6"
-        >
+          className="hover:text-red-500 p-6 mb-6">
           <ImCross className="text-2xl" />
         </button>
         <hr className="text-xl font-bold h-2 mb-3" />
         <li
           onClick={() => displayOnOff()}
-          className="md:hover:text-blue-300 px-6 transition-all"
-        >
+          className="md:hover:text-blue-300 px-6 transition-all">
           <Link href="/">Home</Link>
         </li>
         <li
           onClick={() => displayOnOff()}
-          className="md:hover:text-blue-300 px-6 transition-all"
-        >
+          className="md:hover:text-blue-300 px-6 transition-all">
           <Link href="/about">About</Link>
         </li>
         <li
           onClick={() => displayOnOff()}
-          className="md:hover:text-blue-300 px-6 transition-all"
-        >
+          className="md:hover:text-blue-300 px-6 transition-all">
           <Link href="/contact">Contacts</Link>
         </li>
         <li
           onClick={() => displayOnOff()}
-          className="md:hover:text-blue-300 px-6 transition-all"
-        >
+          className="md:hover:text-blue-300 px-6 transition-all">
           <Link href="/pricing">Pricing</Link>
         </li>
 
-        <hr className="mt-5" />
-        <li className="py-2 mt-5 px-4 bg-blue-700 border-0 mx-4 text-white rounded-md hover:text-white hover:scale-105 transition-all text-center">
-          <button>Login</button>
-        </li>
+        {/* <hr className="mt-5" /> */}
+        {/* <li className="py-2 mt-5 px-4 bg-blue-700 border-0 mx-4 text-white rounded-md hover:text-white hover:scale-105 transition-all text-center">
+        </li> */}
+        
       </ul>
     </menu>
   );
