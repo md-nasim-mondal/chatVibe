db9ef80e26f0f05e0f4ba0ae02ecfe7fa6b4c6f5
asshikhon <asshikhon@gmail.com>
2024-10-06 17:22:39 +0600

    added landingPage navbar

--- components/landingPage/Banner.tsx
+++ components/landingPage/Banner.tsx
@@ -1,9 +1,11 @@
+
 import React from "react";
+import Nav from "./Navbar/Nav";
 
 const Banner = () => {
   return (
     <div>
-      <h3 className="text-3xl text-white text-center">This is Banner.</h3>
+   <Nav />
     </div>
   );
 };
---
+++ components/landingPage/Navbar/Nav.tsx
@@ -0,0 +1,68 @@
+"use client"
+import Link from "next/link";
+import React from "react";
+import MobileMenu from "./MobileMenu";
+import { IoNotifications } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { UserButton, useUser } from "@clerk/nextjs";
+function Nav() {
+
+const {user, isLoaded} = useUser() || {};
+
+console.log(user);
+
+  return (
+
+    <nav className="text-[#1973e8] bg-[#d8e0fc80] border-b-2  border-black">
+        <div className="flex justify-between items-center container mx-auto">
+        <a href="/" className="text-center p-5 font-bold text-3xl text-blue-600">
+        Chat<span className="">Vibe</span>
+      </a>
+
+  
+
+      {/* desktop menu */}
+      <menu className="hidden md:flex items-center gap-20 text-[#827F7F] font-semibold mr-6">
+        <ul className="flex gap-4">
+          <li className="hover:text-blue-600 transition-all ">
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="hover:text-blue-600 transition-all ">
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li className="hover:text-blue-600 transition-all ">
+            <Link href={"/contact"}>ContactUs</Link>
+          </li>
+          <li className="hover:text-blue-600 transition-all ">
+            <Link href={"/pricing"}>Pricing</Link>
+          </li>
+        </ul>
+
+      </menu>
+
+
+<div>
+{user ? 
+      <div className="hidden md:block">
+         <UserButton />
+      </div>
+       :  <ul className=" hidden md:flex items-center gap-1 ">
+
+            <li className=" py-2  px-4 text-xl text-white rounded-md bg-blue-500 hover:text-white  hover:scale-105 transition-all">
+         <Link href={`/sign-in`}>
+         <button>logIn</button>
+         </Link>
+          </li>
+        </ul> }
+</div>
+     {/* mobile menu */}
+     <MobileMenu />
+        </div>
+
+       
+   
+    </nav>
+  );
+}
+
+export default Nav;
