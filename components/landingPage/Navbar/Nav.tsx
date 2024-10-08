7918bf86c5833ff6bedf5478968053633eea6e8f
md-nasim-mondal <nasim.programmer24.com>
2024-10-08 11:20:40 +0600

    update navbar path serial for landing page

--- components/landingPage/Navbar/Nav.tsx
+++ components/landingPage/Navbar/Nav.tsx
@@ -13,31 +13,30 @@ function Nav() {
   console.log(user);
 
   return (
-    <nav className="text-[#1973e8] bg-dark-1 shadow-lg relative flex justify-between items-center pr-2 md:pl-0 py-5 md:py-0">
-      <div className="flex justify-between items-center container mx-auto">
+    <nav className='text-[#1973e8] bg-dark-1 shadow-lg relative flex justify-between items-center pr-2 md:pl-0 py-5 md:py-0'>
+      <div className='flex justify-between items-center container mx-auto'>
         <a
-          href="/"
-          className="text-center md:p-5 font-bold text-3xl text-blue-600"
-        >
-          Chat<span className="">Vibe</span>
+          href='/'
+          className='text-center md:p-5 font-bold text-3xl text-blue-600'>
+          Chat<span className=''>Vibe</span>
         </a>
 
         {/* desktop menu */}
-        <menu className="hidden md:flex items-center gap-20 text-white font-semibold mr-6">
-          <ul className="flex gap-4">
-            <li className="hover:text-blue-600 transition-all ">
+        <menu className='hidden md:flex items-center gap-20 text-white font-semibold mr-6'>
+          <ul className='flex gap-4'>
+            <li className='hover:text-blue-600 transition-all '>
               <Link href={"/"}>Home</Link>
             </li>
-            <li className="hover:text-blue-600 transition-all ">
-              <Link href={"/about"}>About</Link>
-            </li>
-            <li className="hover:text-blue-600 transition-all ">
-              <Link href={"/contact"}>Contact</Link>
-            </li>
-            <li className="hover:text-blue-600 transition-all ">
+            <li className='hover:text-blue-600 transition-all '>
               <Link href={"/pricing"}>Pricing</Link>
             </li>
-            <li className="hover:text-blue-600 transition-all ">
+            <li className='hover:text-blue-600 transition-all '>
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li className='hover:text-blue-600 transition-all '>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+            <li className='hover:text-blue-600 transition-all '>
               <Link href={"/dashboard"}>Dashboard</Link>
             </li>
           </ul>
@@ -45,14 +44,12 @@ function Nav() {
 
         <div>
           {user ? (
-            <div className="hidden md:block">
-
+            <div className='hidden md:block'>
               <UserButton />
-
             </div>
           ) : (
-            <ul className=" hidden md:flex items-center gap-1 ">
-              <li className=" py-2  px-4 text-xl text-white rounded-md bg-blue-500 hover:text-white  hover:scale-105 transition-all">
+            <ul className=' hidden md:flex items-center gap-1 '>
+              <li className=' py-2  px-4 text-xl text-white rounded-md bg-blue-500 hover:text-white  hover:scale-105 transition-all'>
                 <Link href={`/sign-in`}>
                   <button>logIn</button>
                 </Link>
@@ -63,7 +60,7 @@ function Nav() {
         {/* mobile menu */}
         {/* <MobileMenu /> */}
       </div>
-      <div className="md:hidden left-0 top-[72px]">
+      <div className='md:hidden left-0 top-[72px]'>
         {/* <Sidebar /> */}
         <SmallNav />
       </div>
