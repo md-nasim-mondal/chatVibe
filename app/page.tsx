652b3a039e6ef788127a3251e69b12f46d58ea18
md-nasim-mondal <nasim.programmer24.com>
2024-10-08 14:53:24 +0600

    added communication.tsx file for communication landing page section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import React from "react";
+import Communication from "@/components/landingPage/Section/Communication";
 import AboutUs from "@/components/landingPage/Section/AboutUs";
 import Banner from "@/components/landingPage/Section/Banner";
-import Nav from "@/components/landingPage/Navbar/Nav";
-import React from "react";
 
 const LandingPage = () => {
   return (
     <div>
       <Banner />
+      <Communication />
       <AboutUs />
     </div>
   );
---
+++ components/landingPage/Section/Communication.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import SectionContainer from "../ShareComponents/SectionContainer";
+
+const Communication = () => {
+  return (
+    <SectionContainer>
+      <div className='flex flex-col md:flex-row justify-around items-center gap-8'>
+        <div className='md:flex-1 lg:flex-initial'>
+          <Image
+            src={`https://i.ibb.co.com/M5GtMCV/message.png`}
+            alt='Secure Conversations'
+            className='lg:w-[460px] lg:h-[460px]  mt-6 object-cover border-0 rounded-xl'
+            width={320}
+            height={320}
+          />
+        </div>
+
+        <div className='flex flex-col justify-center md:flex-1 lg:flex-initial'>
+          <h2 className='text-4xl font-bold'>
+            <span className='text-4xl font-bold text-blue-500'>
+              Communication
+            </span>{" "}
+            with Live Discussions
+          </h2>
+          <p className='text-base text-[#677471] lg:w-[500px] pt-4'>
+            Discuss directly with the team, you can send document files for the
+            next level of collaboration.
+          </p>
+          <button className='btn border-0 text-white text-base mt-4 bg-blue-500 '>
+            Learn More
+          </button>
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
+
+export default Communication;
