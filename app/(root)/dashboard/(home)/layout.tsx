653f98cf61434e4708953c9cbfb606f81fc32ec4
Md Atikuzzaman <akash203037@gmail.com>
2024-10-10 19:29:10 +0600

    icons modified

--- app/(root)/dashboard/(home)/layout.tsx
+++ app/(root)/dashboard/(home)/layout.tsx
@@ -7,18 +7,18 @@ export const metadata: Metadata = {
   title: "ChatVibe",
   description: "video calling app",
   icons: {
-    icon: "/icons/logo.svg",
+    icon: "/images/logo.png",
   },
 };
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className='relative'>
+    <main className="relative">
       <Navbar />
-      <div className='flex'>
+      <div className="flex">
         <Sidebar />
-        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-          <div className='w-full'>{children}</div>
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
         </section>
       </div>
       Footer
