04edaca331f7097d920a261a72e0796d4b6cb91b
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-10-23 14:56:16 +0600

    banner update

--- components/landingPage/Section/Banner.tsx
+++ components/landingPage/Section/Banner.tsx
@@ -11,26 +11,24 @@ const Banner = () => {
     {
       text: "Get a Link You can share",
       description: "Click new meeting to create a new meeting and share the link",
-      img: "https://i.ibb.co/SfpWJdQ/images.jpg",
+      img: "https://www.gstatic.com/meet/premium_carousel_02_174e55774263506d1280ce6552233189.gif",
     },
     {
       text: "Connect with anyone",
       description: "Invite your team members to join the meeting via a shared link.",
-      img: "https://i.ibb.co/ynh16GR/memory.png",
+      img: "https://i.ibb.co.com/0nvdBq6/isometric-recruiting-icon-with-male-recruiter-communicating-with-two-job-candidates-round-office-tab.png",
     },
   ];
-
 
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, 3000);
+    }, 2000); // Change interval to 2000 milliseconds (2 seconds)
     return () => clearInterval(interval);
   }, [slides.length]);
 
-
   const slideVariants = {
-    enter: (direction: number) => ({
+    enter: (direction) => ({
       x: direction > 0 ? 1000 : -1000,
       opacity: 0,
     }),
@@ -38,7 +36,7 @@ const Banner = () => {
       x: 0,
       opacity: 1,
     },
-    exit: (direction: number) => ({
+    exit: (direction) => ({
       x: direction < 0 ? 1000 : -1000,
       opacity: 0,
     }),
@@ -48,13 +46,12 @@ const Banner = () => {
     <div className="overflow-x-hidden">
       <SectionContainer>
         <div className="flex flex-col-reverse md:flex-row items-center justify-evenly">
-
           <motion.div
             className="h-full flex flex-col justify-center text-left md:p-4 lg:p-4 lg:mr-4 space-y-4 lg:space-y-6"
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -50 }} 
-            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.8 }}
           >
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 text-white">
               Meeting and Video Call for <br />
@@ -62,18 +59,18 @@ const Banner = () => {
               <span className="text-main-1"> ChatVibe</span>
             </h1>
             <motion.div
-  className="flex justify-center w-full"
-  whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.9 }}
-  transition={{ type: "spring", stiffness: 300 }}
->
-  <Link
-    href="/dashboard"
-    className="w-full text-center bg-main-2 text-white p-2 md:py-2 md:px-4 rounded-lg hover:bg-main-3 transition duration-300"
-  >
-    Try Now
-  </Link>
-</motion.div>
+              className="flex justify-center w-full"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <Link
+                href="/dashboard"
+                className="w-full text-center bg-main-2 text-white p-2 md:py-2 md:px-4 rounded-lg hover:bg-main-3 transition duration-300"
+              >
+                Try Now
+              </Link>
+            </motion.div>
 
             <p className="mt-10 text-gray-300">
               Learn more about{" "}
@@ -83,22 +80,21 @@ const Banner = () => {
             </p>
           </motion.div>
 
-       
           <motion.div
-            className="flex-1 h-[500px] max-w-full md:max-w-[620px] pb-12 lg:max-w-[720px] lg:ml-8"
+            className="flex-1 h-[500px] max-w-full md:max-w-[620px] pb-12 lg:max-w-[720px] lg:ml-8 relative"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.8 }}
           >
             <motion.div
-              className="bg-[#d9d9d9] p-6 rounded-lg shadow-lg h-full flex flex-col justify-center space-y-4"
+              className="p-6 rounded-lg h-full flex flex-col justify-center space-y-4"
               key={activeSlide}
-              custom={1} 
+              custom={1}
               variants={slideVariants}
               initial="enter"
               animate="center"
               exit="exit"
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.8 }}
             >
               <Image
                 width={208}
@@ -107,26 +103,28 @@ const Banner = () => {
                 alt={slides[activeSlide].text}
                 className="w-52 h-52 rounded-full mx-auto"
               />
-              <p className="mt-4 text-center text-black font-semibold">
+              <p className="mt-4 text-center text-main-1 font-semibold">
                 {slides[activeSlide].text}
               </p>
-              <p className="mt-2 text-center text-sm text-gray-600">
+              <p className="mt-2 text-center text-sm text-white">
                 {slides[activeSlide].description}
               </p>
             </motion.div>
+
+            {/* Arrow controls */}
             <div className="flex justify-between mt-4">
               <span
                 onClick={() =>
                   setActiveSlide((activeSlide - 1 + slides.length) % slides.length)
                 }
-                className="text-main-1 cursor-pointer"
+                className="absolute left-0 top-1/2 transform -translate-y-1/2 text-main-1 cursor-pointer"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  className="w-6 h-6"
+                  className="w-8 h-8"
                 >
                   <path
                     strokeLinecap="round"
@@ -140,14 +138,14 @@ const Banner = () => {
                 onClick={() =>
                   setActiveSlide((activeSlide + 1) % slides.length)
                 }
-                className="text-main-1 cursor-pointer"
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 text-main-1 cursor-pointer"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  className="w-6 h-6"
+                  className="w-8 h-8"
                 >
                   <path
                     strokeLinecap="round"
@@ -157,6 +155,20 @@ const Banner = () => {
                   />
                 </svg>
               </span>
+            </div>
+
+            {/* Pagination Indicators */}
+            <div className="flex justify-center">
+              {slides.map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => setActiveSlide(index)}
+                  className={`w-2 h-2 mx-1 rounded-full ${
+                    activeSlide === index ? "bg-main-1" : "bg-gray-300"
+                  }`}
+                  aria-label={`Slide ${index + 1}`}
+                />
+              ))}
             </div>
           </motion.div>
         </div>
