31d8ecaf1ee031b5eb262ba1b265ef6d6fda3fe0
Md Atikuzzaman <akash203037@gmail.com>
2024-10-24 20:16:03 +0600

    team members portfolio add in progress

--- components/landingPage/ShareComponents/Member.tsx
+++ components/landingPage/ShareComponents/Member.tsx
@@ -1,4 +1,6 @@
+
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface MemberProps {
@@ -6,9 +8,10 @@ interface MemberProps {
   name: string;
   profession: string;
   role: string;
+  url: string;
 }
 
-const Member: React.FC<MemberProps> = ({ src, name, profession, role }) => {
+const Member: React.FC<MemberProps> = ({ src, name, profession, role, url }) => {
   return (
     <div className='flex flex-col items-center justify-between border border-black rounded-md bg-transparent hover:bg-[#aabcd8] hover:scale-105 transition-all h-auto w-auto max-w-[320px] mx-auto'>
       <Image
@@ -18,11 +21,11 @@ const Member: React.FC<MemberProps> = ({ src, name, profession, role }) => {
         width={200}
         className='rounded-t-lg min-w-[256px] min-h-[256px]'
       />
-      <div className='bg-main-2 hover:bg-main-3 w-full text-white hover:text-[] p-2 rounded-b-lg'>
+      <Link href={'https://www.google.com'} className='bg-main-2 hover:bg-main-3 w-full text-white hover:text-[] p-2 rounded-b-lg'>
         <h3 className='text-2xl font-semibold'>{name}</h3>
         <p className='font-semibold'>{profession}</p>
         <p className='font-semibold'>{role}</p>
-      </div>
+      </Link>
     </div>
   );
 };
