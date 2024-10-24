ce5e219a04d7cc89b80201e46371e78ea1d607ff
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-10-24 14:37:55 +0600

    added update

--- app/payment/CheckoutForm.tsx
+++ app/payment/CheckoutForm.tsx
@@ -48,21 +48,18 @@ const CheckoutForm = ({ price }) => {
       <ToastContainer />
 
       <form onSubmit={handleSubmit}>
-      <div className="flex items-center justify-center ">
-        <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-center text-gray-700 text-lg font-semibold mb-4">
-            Total price
-          </h2>
-          <div className="my-4">
-            <h3 className="text-4xl text-black  font-semibold text-center">
-              {" "}
-              ${price}
-            </h3>
-          </div>
-          <div className="space-y-2 rounded-md  bg-black p-8">
-           
-              
-
+        <div className="flex items-center justify-center ">
+          <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-center text-gray-700 text-lg font-semibold mb-4">
+              Total price
+            </h2>
+            <div className="my-4">
+              <h3 className="text-4xl text-black  font-semibold text-center">
+                {" "}
+                ${price}
+              </h3>
+            </div>
+            <div className="space-y-2 rounded-md  bg-black p-8">
               <CardElement
                 options={{
                   style: {
@@ -79,28 +76,27 @@ const CheckoutForm = ({ price }) => {
                   },
                 }}
               />
-                          
-          </div>
-          <p className="text-red-600 mb-8">{ error}</p>
-          <div className="flex flex-col items-center bg-gray-100 p-4 rounded-md mb-6">
-            <RiSecurePaymentFill className="text-black text-5xl"></RiSecurePaymentFill>
-            <p className="text-center text-sm text-gray-600">
-              Your payment is 100% safe and secure. We use encryption to protect
-              your data and only work with verified payment gateways. Your trust
-              is a priority for us.
-            </p>
-          </div>
+            </div>
+            <p className="text-red-600 mb-8">{error}</p>
+            <div className="flex flex-col items-center bg-gray-100 p-4 rounded-md mb-6">
+              <RiSecurePaymentFill className="text-black text-5xl"></RiSecurePaymentFill>
+              <p className="text-center text-sm text-gray-600">
+                Your payment is 100% safe and secure. We use encryption to
+                protect your data and only work with verified payment gateways.
+                Your trust is a priority for us.
+              </p>
+            </div>
 
-          <button
-            type="submit"
-            disabled={!stripe}
-            className="w-full bg-lime-500 text-white font-semibold py-3 rounded-lg hover:bg-lime-600 transition duration-300"
-          >
-            Pay ${price} &rarr;
-          </button>
+            <button
+              type="submit"
+              disabled={!stripe}
+              className="w-full bg-lime-500 text-white font-semibold py-3 rounded-lg hover:bg-lime-600 transition duration-300"
+            >
+              Pay ${price} &rarr;
+            </button>
+          </div>
         </div>
-              </div>
-              </form>
+      </form>
     </div>
   );
 };
--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,29 +1,3 @@
-// "use client"
-// import { useRouter } from "next/router";
-// import { Elements } from "@stripe/react-stripe-js";
-// import { loadStripe } from "@stripe/stripe-js";
-// import CheckoutForm from "./CheckoutForm";
-
-// // const stripePromise =loadStripe(import.meta.env.NEXT_PAYMENT_GATEWAY_PK)
-// const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_API_KEY}`);
-
-
-// const Page = () => {
-//     const router = useRouter();
-//     const { price } = router.query;
-
-//     return (
-//         <div className="mt-60">
-//       <h2 className="text-white text-center mb-8 text-3xl">Checkout - {price ? `$${price}` : 'No Price Selected'}</h2>
-//       <Elements stripe={stripePromise}>
-//                 <CheckoutForm price={ price} />
-//       </Elements>
-//     </div>
-//     );
-// };
-
-// export default Page;
-
 "use client";
 import { useSearchParams } from "next/navigation"; // Use `next/navigation` for `useSearchParams`
 import { Elements } from "@stripe/react-stripe-js";
