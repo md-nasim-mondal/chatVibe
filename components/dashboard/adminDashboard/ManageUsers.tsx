4cfa8cdff8fa4dd82c20ced6bb3327826994dc3d
md-nasim-mondal <nasim.programmer24@gmail.com>
2024-10-25 00:47:24 +0600

    Try to fixed user role update ui

--- components/dashboard/adminDashboard/ManageUsers.tsx
+++ components/dashboard/adminDashboard/ManageUsers.tsx
@@ -25,7 +25,9 @@ const ManageUsers: React.FC = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/all`);
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/all`
+        );
         setUsers(response.data);
       } catch (err) {
         setError("Failed to fetch users.");
@@ -37,7 +39,11 @@ const ManageUsers: React.FC = () => {
     fetchUsers();
   }, []);
 
-  const handleRoleChangeFrontend = async (userId: string, email: string, newRole: string) => {
+  const handleRoleChangeFrontend = async (
+    userId: string,
+    email: string,
+    newRole: string
+  ) => {
     try {
       if (newRole === "premiumUser") {
         Swal.fire("Premium user feature coming soon!");
@@ -79,56 +85,77 @@ const ManageUsers: React.FC = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen text-white">
+      <div className='flex justify-center items-center h-screen text-white'>
         <Loader />
       </div>
     );
   }
 
   if (error) {
-    return <div className="text-red-500 text-center">{error}</div>;
+    return <div className='text-red-500 text-center'>{error}</div>;
   }
 
   return (
-    <div className="min-h-screen bg-[#161925] text-white p-4">
-      <h1 className="text-2xl font-bold mb-6 text-center">All Users</h1>
+    <div className='min-h-screen bg-[#161925] text-white p-4'>
+      <h1 className='text-2xl font-bold mb-6 text-center'>All Users</h1>
 
       {/* Table for Medium and Large Devices */}
-      <div className="overflow-x-auto p-4 hidden md:block">
-        <table className="min-w-full table-auto bg-gray-800 border-separate border-spacing-y-2">
+      <div className='overflow-x-auto p-4 hidden md:block'>
+        <table className='min-w-full table-auto bg-gray-800 border-separate border-spacing-y-2'>
           <thead>
-            <tr className="bg-main-2 text-left">
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">#</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Image</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Full Name</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Email</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Role</th>
+            <tr className='bg-main-2 text-left'>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>#</th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Image
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Full Name
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Email
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Role
+              </th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <tr key={user._id} className="hover:bg-[#1C1F2E] hover:text-white transition">
-                <td className="py-2 px-4">{index + 1}</td>
-                <td className="py-2 px-4">
+              <tr
+                key={user._id}
+                className='hover:bg-[#1C1F2E] hover:text-white transition'>
+                <td className='py-2 px-4'>{index + 1}</td>
+                <td className='py-2 px-4'>
                   <img
                     src={user.imageUrl}
                     alt={user.fullName}
-                    className="w-10 h-10 rounded-full"
+                    className='w-10 h-10 rounded-full'
                   />
                 </td>
-                <td className="py-2 px-4 text-sm">{user.fullName}</td>
-                <td className="py-2 px-4 text-sm">{user.emailAddresses}</td>
-                <td className="py-2 px-4 text-sm">
+                <td className='py-2 px-4 text-sm'>{user.fullName}</td>
+                <td className='py-2 px-4 text-sm'>{user.emailAddresses}</td>
+                <td className='py-2 px-4 text-sm'>
                   <select
-                    value={user.role}
+                    value={user?.role}
                     onChange={(e) =>
-                      handleRoleChangeFrontend(user._id, user.emailAddresses, e.target.value)
+                      handleRoleChangeFrontend(
+                        user._id,
+                        user.emailAddresses,
+                        e.target.value
+                      )
                     }
-                    className="bg-gray-700 text-white rounded px-2 py-1"
-                  >
-                    <option value="user">User</option>
-                    <option value="premiumUser">Premium User</option>
-                    <option value="admin">Admin</option>
+                    className='bg-gray-700 text-white rounded px-2 py-1'>
+                    <option value='user' selected={user?.role === "user"}>
+                      User
+                    </option>
+                    <option
+                      value='premiumUser'
+                      selected={user?.role === "premiumUser"}>
+                      Premium User
+                    </option>
+                    <option value='admin' selected={user?.role === "admin"}>
+                      Admin
+                    </option>
                   </select>
                 </td>
               </tr>
@@ -138,43 +165,58 @@ const ManageUsers: React.FC = () => {
       </div>
 
       {/* Mobile-Friendly Card Layout for Small Devices */}
-      <div className="overflow-x-auto p-4 md:hidden">
-        <table className="min-w-full table-auto bg-gray-800 border-separate border-spacing-y-2">
+      <div className='overflow-x-auto p-4 md:hidden'>
+        <table className='min-w-full table-auto bg-gray-800 border-separate border-spacing-y-2'>
           <thead>
-            <tr className="bg-main-2 text-left">
-              <th className="py-3 px-4 text-sm font-medium text-gray-300 hidden md:block">#</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300 hidden md:block">Image</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Full Name</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300 hidden md:block">Email</th>
-              <th className="py-3 px-4 text-sm font-medium text-gray-300">Role</th>
+            <tr className='bg-main-2 text-left'>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300 hidden md:block'>
+                #
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300 hidden md:block'>
+                Image
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Full Name
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300 hidden md:block'>
+                Email
+              </th>
+              <th className='py-3 px-4 text-sm font-medium text-gray-300'>
+                Role
+              </th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <tr key={user._id} className="hover:bg-[#1C1F2E] hover:text-white transition">
-                <td className="py-2 px-4 hidden md:block">{index + 1}</td>
-                <td className="py-2 px-4 hidden md:block">
+              <tr
+                key={user._id}
+                className='hover:bg-[#1C1F2E] hover:text-white transition'>
+                <td className='py-2 px-4 hidden md:block'>{index + 1}</td>
+                <td className='py-2 px-4 hidden md:block'>
                   <img
                     src={user.imageUrl}
                     alt={user.fullName}
-                    className="w-10 h-10 rounded-full"
+                    className='w-10 h-10 rounded-full'
                   />
                 </td>
-                <td className="py-2 px-4 text-sm">{user.fullName}</td>
-                <td className="py-2 px-4 text-sm hidden md:block">
+                <td className='py-2 px-4 text-sm'>{user.fullName}</td>
+                <td className='py-2 px-4 text-sm hidden md:block'>
                   {user.emailAddresses}
                 </td>
-                <td className="py-2 px-4 text-sm">
+                <td className='py-2 px-4 text-sm'>
                   <select
                     value={user.role}
                     onChange={(e) =>
-                      handleRoleChangeFrontend(user._id, user.emailAddresses, e.target.value)
+                      handleRoleChangeFrontend(
+                        user._id,
+                        user.emailAddresses,
+                        e.target.value
+                      )
                     }
-                    className="bg-gray-700 text-white rounded px-2 py-1"
-                  >
-                    <option value="user">User</option>
-                    <option value="premiumUser">Premium User</option>
-                    <option value="admin">Admin</option>
+                    className='bg-gray-700 text-white rounded px-2 py-1'>
+                    <option value='user'>User</option>
+                    <option value='premiumUser'>Premium User</option>
+                    <option value='admin'>Admin</option>
                   </select>
                 </td>
               </tr>
